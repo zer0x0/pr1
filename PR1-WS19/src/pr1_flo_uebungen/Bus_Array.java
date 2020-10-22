e0f558a183da0a2390e8876ec67cfc57a28d288b
zer0x0 <zer0x0@pm.me>
2020-10-22 22:28:51 +0200

    test

--- PR1-WS19/src/pr1_flo_uebungen/Bus_Array.java
+++ PR1-WS19/src/pr1_flo_uebungen/Bus_Array.java
@@ -1,11 +1,8 @@
 package pr1_flo_uebungen;
 
-import static pr.MakeItSimple.*;
-
 public class Bus_Array {
 
 	public static void main(String[] args) {
-		// TODO Automatisch generierter Methodenstub
 
 	}
 
