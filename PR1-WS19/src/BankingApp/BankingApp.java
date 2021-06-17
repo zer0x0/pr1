900b61b302919fde99161dbdf309f3e14b1fce72
[zer0x0] <zer0x0@icloud.com>
2021-06-17 22:55:05 +0200

    Test API

--- PR1-WS19/src/BankingApp/BankingApp.java
+++ PR1-WS19/src/BankingApp/BankingApp.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class BankingApp {
 
 	public static void main(String[] args) {
-		
-		BankAccount obj1 = new BankAccount("Florian Kaffenberger","CZ-1890-A");
+
+		BankAccount obj1 = new BankAccount("Florian Kaffenberger", "CZ-1890-A");
 		obj1.showMenu();
-		
+
 	}
 
 }
 
 class BankAccount {
-
 	int balance;
 	int previousTransaction;
 	String customerName;
@@ -24,7 +23,6 @@ class BankAccount {
 	/*
 	 * Konstruktor
 	 */
-
 
 	BankAccount(String cname, String cid) {
 
@@ -61,9 +59,9 @@ class BankAccount {
 
 	void getPreviousTransaction() {
 		if (previousTransaction > 0) {
-			System.out.println("Deposit: " + previousTransaction+"€");
+			System.out.println("Deposit: " + previousTransaction + "€");
 		} else if (previousTransaction < 0) {
-			System.out.println("Withdrawn: " + Math.abs(previousTransaction)+"€");
+			System.out.println("Withdrawn: " + Math.abs(previousTransaction) + "€");
 		} else {
 			System.out.println("No transaction occured.");
 		}
@@ -73,20 +71,20 @@ class BankAccount {
 	/*
 	 * Menügestaltung
 	 */
-	
+
 	void showMenu() {
 
 		/*
 		 * char wird bei position 0 belegt mit User Eingabe (scanner.in)
 		 */
-		
+
 		char option = '\0';
 		scanner = new Scanner(System.in);
 
 		/*
 		 * Anzeige des Menüs
 		 */
-		
+
 		System.out.println("Welcome " + customerName);
 		System.out.println("Your ID is " + customerID);
 		System.out.println("\n");
@@ -99,23 +97,25 @@ class BankAccount {
 		/*
 		 * Auswahl der verschiedenen Möglichkeiten im Menu
 		 */
-		
+
 		do {
-			System.out.println("==========================================================================================================");
+			System.out.println(
+					"==========================================================================================================");
 			System.out.println("Enter an option");
-			System.out.println("==========================================================================================================");
+			System.out.println(
+					"==========================================================================================================");
 			option = scanner.next().charAt(0);
 			System.out.println("\n");
 
 			/*
 			 * Verschiedene Möglichkeiten im Men
 			 */
-			
+
 			switch (option) {
 
 			case 'A':
 				System.out.println("------------------------------------");
-				System.out.println("Balance = " + balance+"€");
+				System.out.println("Balance = " + balance + "€");
 				System.out.println("------------------------------------");
 				System.out.println("\n");
 				break;
@@ -124,11 +124,11 @@ class BankAccount {
 				System.out.println("------------------------------------");
 				System.out.println("Enter an amount to deposit:");
 				System.out.println("------------------------------------");
-				
+
 				/*
 				 * Input wird eingelesen und in der deposit methode verwendet
 				 */
-				
+
 				int amount = scanner.nextInt();
 				deposit(amount);
 				System.out.println("\n");
@@ -138,11 +138,11 @@ class BankAccount {
 				System.out.println("------------------------------------");
 				System.out.println("Enter amount of withdraw:");
 				System.out.println("------------------------------------");
-				
+
 				/*
 				 * Input wird eingelesen und von der withdraw methode verwendet
 				 */
-				
+
 				int amount2 = scanner.nextInt();
 				withdraw(amount2);
 				System.out.println("\n");
@@ -150,11 +150,11 @@ class BankAccount {
 
 			case 'D':
 				System.out.println("------------------------------------");
-				
+
 				/*
 				 * Methode getPreviousTransaction wird aufgerufen
 				 */
-				
+
 				getPreviousTransaction();
 				System.out.println("------------------------------------");
 				System.out.println("\n");
@@ -163,11 +163,12 @@ class BankAccount {
 			case 'E':
 				System.out.println("****************************************");
 				break;
-			
+
 			/*
-			 * wird ein CHARakter eingegeben der nicht in als case angegeben ist so wird Invalid Option.. ausgegeben
+			 * wird ein CHARakter eingegeben der nicht in als case angegeben ist so wird
+			 * Invalid Option.. ausgegeben
 			 */
-			
+
 			default:
 				System.out.println("Invalid Option ! Please enter again.");
 				break;
@@ -176,7 +177,7 @@ class BankAccount {
 			/*
 			 * Loop wird beendet wenn E eingegeben wird
 			 */
-			
+
 		} while (option != 'E');
 
 		System.out.println("Thank you for using our services");
